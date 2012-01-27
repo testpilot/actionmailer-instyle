fc40c2ca48733e4a8efa2ac5d319146eb9c49008
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-01-27 16:24:49 +1100

    Updated expectations of CSS Helper spec

--- spec/lib/action_mailer/inline/css_helper_spec.rb
+++ spec/lib/action_mailer/inline/css_helper_spec.rb
@@ -3,8 +3,7 @@ require "spec_helper"
 describe ActionMailer::Inline do
   describe "CSS" do
     it "Should return a CSS String" do
-      Rails.application.assets.find_asset('notification_mailer.css.scss').body.should ==
-        "body {\n  font-family: 'Helvetica Neue', Helvetica, Ariel, sans-serif;\n  font-size: 13px;\n  color: #4d4d4d; }\n"
+      Rails.application.assets.find_asset('notification_mailer.css.scss').body.should match /font-family: 'Helvetica Neue', Helvetica, Ariel, sans-serif/
     end
   end
 end
