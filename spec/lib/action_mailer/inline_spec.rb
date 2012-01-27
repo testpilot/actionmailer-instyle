66c5b509c797a677740d0a7656cd76f1cfdd5a4c
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-01-27 16:24:16 +1100

    Filled in examples for inline_spec

--- spec/lib/action_mailer/inline_spec.rb
+++ spec/lib/action_mailer/inline_spec.rb
@@ -5,21 +5,44 @@ describe ActionMailer::Inline do
     Mail.class_variable_get("@@delivery_interceptors").should include ActionMailer::Inline
   end
 
-  it "should return text version of email if not present"
+  let(:original_message) { NotificationMailer.welcome_html_email.deliver }
+  let(:message) { ActionMailer::Inline.delivering_email(original_message) }
 
-  it "should replace html version with premailer processed inline version"
+  it "should keep text version of email if present" do
+    original_message.text_part.body.to_s.should == message.text_part.body.to_s
+  end
 
-  it "should find stylesheet assets"
+  it "should have two body parts" do
+    message.parts.size.should == 2
+  end
 
-  it "should load stylesheet assets using asset pipeline"
+  it "should not have a style tag in original email" do
+    ActionMailer::Inline.should_receive(:delivering_email).once
+    original_message = NotificationMailer.welcome_html_email.deliver
+    Nokogiri::HTML(original_message.html_part.body.to_s).css('body').first.attributes['style'].should be_nil
+  end
 
-  it "should fallback to email.css if no stylesheets are present"
+  it "should replace html version with premailer processed inline version" do
+    # If it has a style attribue then we have processed it
+    Nokogiri::HTML(message.html_part.body.to_s).css('body').first.attributes['style'].should_not be_nil
+  end
 
-  it "should not modify text part if already present"
+  it "should find stylesheet assets" do
 
-  it "should inline stylesheets and replace IDs and Classes"
+  end
 
-  it "should create a text part from the html part if not present"
+  it "should not do anything if no stylesheets are present" do
+    message_with_no_css = ActionMailer::Inline.delivering_email(NotificationMailerNoStyle.email_with_no_style.deliver)
+    Nokogiri::HTML(message_with_no_css.html_part.body.to_s).css('body').first.attributes['style'].should be_nil
+  end
 
+  it "should inline stylesheets and replace IDs and Classes" do
+    # According to the email template the first table usually has a 'striped' class
+    Nokogiri::HTML(message.html_part.body.to_s).css('table').first.attributes['class'].should be_nil
+  end
 
+  it "should create a text part from the html part if not present" do
+    message = ActionMailer::Inline.delivering_email(NotificationMailer.email_with_html_only.deliver)
+    message.text_part.body.to_s.should == "Welcome, this is a HTML email\n\nAnother line"
+  end
 end
