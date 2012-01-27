9247212720a6fe03677b0d71a251292c9ad59b03
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-01-27 16:25:39 +1100

    Cleaned up Inline

--- lib/action_mailer/inline.rb
+++ lib/action_mailer/inline.rb
@@ -3,8 +3,17 @@ require "action_mailer/base"
 
 module ActionMailer
   module Inline
+    autoload :Processor, 'action_mailer/inline/processor'
+    autoload :Premailer, 'action_mailer/inline/premailer'
+
+    # delivering_email hook within Mail
+    #
+    # This is where the whole process kicks off.
     def self.delivering_email(message)
-      
+      # If the email contains a html part or is only html
+      if message.html_part || (message.content_type =~ /text\/html/ && message)
+        Inline::Processor.inline!(message)
+      end
     end
   end
 end
