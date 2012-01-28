471e2c61d5f7f2a34b27057b812a232bdebbf3c5
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-01-28 15:19:20 +1100

    Fixed hash syntax in internal app

--- spec/internal/app/mailers/notification_mailer_no_layout.rb
+++ spec/internal/app/mailers/notification_mailer_no_layout.rb
@@ -1,10 +1,9 @@
 class NotificationMailerNoStyle < ActionMailer::Base
   layout "notification_mailer_without_style"
 
-  default from: "Johnny Quids<quidlicker@example.com>"
+  default :from => "Johnny Quids<quidlicker@example.com>"
 
   def email_with_no_style
-    mail(to: "archie@example.com", subject: "We need dry ice")
+    mail(:to => "archie@example.com", :subject => "We need dry ice")
   end
 end
-
